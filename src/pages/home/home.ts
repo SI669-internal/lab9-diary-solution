e284d70ae4d992d780b7f602c7ce886e6f6fa370
Newman <mwnewman@m-c02sp0syh03m.local>
2018-10-30 22:42:49 -0400

    step 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EntryDetailPage } from '../entry-detail/entry-detail';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  go() {
+    this.navCtrl.push(EntryDetailPage);
+  }
 }
